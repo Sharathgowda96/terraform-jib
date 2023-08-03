cb1db6f7284d45ba839eb4881753f48350eed86f
Sharathgowda96 <85106974+Sharathgowda96@users.noreply.github.com>
2023-08-03 09:36:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,59 @@
 resource "aws_s3_bucket" "onebucket" {
-   bucket = "testing-s3-with-terraform"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   tags = {
-     Name = "Bucket1"
-     Environment = "Test"
-   }
+  bucket = "testing-s3-with-terra"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Name        = "Bucket1"
+    Environment = "Test"
+  }
 }
+
+resource "aws_iam_policy" "s3_full_access" {
+  name = "s3_full_access"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::testing-s3-with-terraform",
+        "arn:aws:s3:::testing-s3-with-terraform/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "s3_role" {
+  name = "S3FullAccessRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "s3_full_access_attachment" {
+  name       = "s3_full_access_attachment"
+  policy_arn = aws_iam_policy.s3_full_access.arn
+  roles      = [aws_iam_role.s3_role.name]
+}
+
