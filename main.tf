eed6f00defe65e7b7e0df80488c8977bede655ec
Sharathgowda96 <85106974+Sharathgowda96@users.noreply.github.com>
2023-08-03 15:37:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_role" "s3_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "ec2.amazonaws.com"
+          Service = "s3.amazonaws.com"
         }
       }
     ]
