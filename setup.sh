f07217af150b12f44ed8bd1c6d64d82ac5cff93f
Sharathgowda96 <85106974+Sharathgowda96@users.noreply.github.com>
2023-08-09 16:11:25 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Configure AWS credentials
+aws configure set aws_access_key_id "$Access_Key"
+aws configure set aws_secret_access_key "$Secret_Access_Key"
+aws configure set default.region "us-west-1"  # Replace with your desired region
+
+# Install Terraform
+curl "https://releases.hashicorp.com/terraform/1.0.0/terraform_1.0.0_linux_amd64.zip" -o "terraform.zip"
+unzip terraform.zip
+sudo mv terraform /usr/local/bin/
