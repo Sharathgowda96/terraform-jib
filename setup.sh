349c3fbc4bb09825843bd802e12a4956c82232c9
Sharathgowda96 <85106974+Sharathgowda96@users.noreply.github.com>
2023-10-23 14:07:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-# Configure AWS credentials
+# Configure AWS credential
 aws configure set aws_access_key_id "$Access_Key"
 aws configure set aws_secret_access_key "$Secret_Access_Key"
 aws configure set default.region "us-west-1"  # Replace with your desired region
